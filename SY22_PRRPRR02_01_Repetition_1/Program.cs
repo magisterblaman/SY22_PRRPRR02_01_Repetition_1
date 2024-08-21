44b5284bbb5d2671ca9a3ebbf67deba468470643
hannesblaman <hannes@blaman.com>
2024-08-21 13:24:28 +0200

    Grundläggande output + strings

--- SY22_PRRPRR02_01_Repetition_1/Program.cs
+++ SY22_PRRPRR02_01_Repetition_1/Program.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace SY22_PRRPRR02_01_Repetition_1 {
 	internal class Program {
 		static void Main(string[] args) {
+			Console.WriteLine("Hello world!");
+
+			string namnetPåVariabeln = "Det här är en string";
+			Console.WriteLine(namnetPåVariabeln);
+
+			namnetPåVariabeln = "Jag är jättecool 123";
+			Console.WriteLine(namnetPåVariabeln);
+
+			string förnamn = "Hannes";
+			string efternamn = "Blåman";
+
+			string helaNamnet = förnamn + " " + efternamn;
+			Console.WriteLine(helaNamnet);
 		}
 	}
 }
