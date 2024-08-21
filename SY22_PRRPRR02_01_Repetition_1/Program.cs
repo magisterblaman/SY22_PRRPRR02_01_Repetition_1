ab6f9b6e1da899c964a6b307e5d34da0fdfe9adc
hannesblaman <hannes@blaman.com>
2024-08-21 14:08:25 +0200

    Operatorer och casting

--- SY22_PRRPRR02_01_Repetition_1/Program.cs
+++ SY22_PRRPRR02_01_Repetition_1/Program.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace SY22_PRRPRR02_01_Repetition_1 {
 	internal class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Skriv ditt personnummer:");
-			string personnummer = Console.ReadLine();
+			int x = 3829;
+			int y = 32;
 
-			if (personnummer == "188603240000") {
-				Console.WriteLine("Du är världens äldsta person, grattis");
-			} else {
-				Console.WriteLine("Synd");
-			}
+			double z = 32.43;
+
+			int sum = (int)(y + z);
+
+			Console.WriteLine(x % y);
 		}
 	}
 }
